1ea9d90137d06c080cf26c9714d2a011d7c7cb7c
Joberth Rogers <joberth.rogers18@gmail.com>
2018-10-07 20:01:20 -0300

    Adding property bind from databind in heart images from attemp
    Create class phrase and create a file with mock phrases for test aplication

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import {Frase} from '../shared/frase.model'
+
+const FRASES: Array<Frase> = [
+    {fraseEng: 'How are you?',frasePtBr:' Como vai voce?'},
+    {fraseEng: 'I like play soccer', frasePtBr: 'Eu gosto de jogar futebol'},
+    {fraseEng: 'The diference between us is the distance', frasePtBr: 'A diferença entre nós é a distância'},
+    {fraseEng:'If you can dream you can do it', frasePtBr: 'Se você pode sonhar você pode fazer'}
+]
